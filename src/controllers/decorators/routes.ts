e28520c2d63793d8e68a5d070453843d0ec73127
nguye374 <nguye374@sheridancollege.ca>
2022-07-10 00:50:17 -0400

    add different method func

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,8 +1,16 @@
 import 'reflect-metadata';
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata('path', path, target, key);
-    // const pathValue = Reflect.getMetadata('path', target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, target, key);
+      Reflect.defineMetadata('method', method, target, key);
+    };
   };
 }
+
+export const get = routeBinder('get');
+export const post = routeBinder('post');
+export const put = routeBinder('put');
+export const del = routeBinder('delete');
+export const patch = routeBinder('patch');
